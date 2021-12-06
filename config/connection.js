aa9cbde58ca802d0704032af5a0f9e364f42109c
umnovjp <umnovjp@yahoo.com>
2021-12-06 14:43:10 -0600

    most routes work but two product routes do not work

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,19 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize(
+  // Database name
+  'ecommerce_db',
+  // User
+  'root',
+  // Password
+  'Virahovs74#',
+  {
+    // Database location
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
 
 module.exports = sequelize;
--- routes/api/product-routes2.js
+++ routes/api/product-routes2.js
@@ -63,7 +63,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-    Product.create(req.body)
+  Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
